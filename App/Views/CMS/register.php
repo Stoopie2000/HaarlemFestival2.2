988f4b7583add2d50a167f5d23e1403f2121c0d8
stoopie <stoopie2000@gmail.com>
2020-01-14 23:45:12 +0100

    Added form feedback to register page

--- App/Views/CMS/register.php
+++ App/Views/CMS/register.php
@@ -20,28 +20,33 @@
         <form action="<?php echo Config::URLROOT; ?>/cms/register" method="post">
             <div class="form-group row">
                 <div class="col">
-                <label for="firstName">First Name:</label>
-                <input type="text" name="firstName" placeholder="First Name" autofocus value="<?php if (isset($firstName)) echo ($firstName);?>" class="form-control" />
+                    <label for="firstName">First Name:</label>
+                    <input type="text" name="firstName" placeholder="First Name" autofocus value="<?php if (isset($firstName)) echo ($firstName);?>" class="form-control <?php if (isset($firstName)) if ($firstName == "") echo "is-invalid";?>" />
+                    <span class="invalid-feedback">Please fill in your first name</span>
                 </div>
                 <div class="col">
-                <label for="lastName">Last Name:</label>
-                <input type="text" name="lastName" placeholder="Last Name" autofocus value="<?php if (isset($lastName)) echo ($lastName);?>" class="form-control" />
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" name="lastName" placeholder="Last Name" autofocus value="<?php if (isset($lastName)) echo ($lastName);?>" class="form-control <?php if (isset($lastName)) if ($lastName == "") echo "is-invalid";?>" />
+                    <span class="invalid-feedback">Please fill in your last name</span>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="inputEmail">Email address:</label>
-                <input type="text" name="email" placeholder="Email address" autofocus value="<?php if (isset($email)) echo ($email);?>" class="form-control" />
+                <input type="text" name="email" placeholder="Email address" autofocus value="<?php if (isset($email)) echo ($email);?>" class="form-control <?php if (isset($email)) if ($email == "") echo "is-invalid";?>" />
+                <span class="invalid-feedback">Please fill in your email address</span>
             </div>
 
             <div class="form-group">
                 <label for="inputPassword">Password:</label>
-                <input type="password" id="inputPassword" name="password" placeholder="Password" class="form-control" />
+                <input type="password" id="inputPassword" name="password" placeholder="Password" class="form-control <?php if (isset($pass_err)) if ($pass_err != "") echo "is-invalid";?>" />
+                <span class="invalid-feedback">please fill in a password</span>
             </div>
 
             <div class="form-group">
                 <label for="confpassword">Confirm Password:</label>
-                <input type="password" id="inputConfPassword" name="confpassword" placeholder="Confirm password" class="form-control" />
+                <input type="password" id="inputConfPassword" name="confpassword" placeholder="Confirm password" class="form-control <?php if (isset($confpass_err)) if ($confpass_err != "") echo "is-invalid";?>" />
+                <span class="invalid-feedback"><?php if (isset($confpass_err)) echo $confpass_err; ?></span>
             </div>
 
             <div class="text-center">
@@ -68,7 +73,7 @@
 
             <div class="form-group">
                 <label for="inputPassword">Password:</label>
-                <input type="password" id="inputPassword" name="password" placeholder="Password" class="form-control" />
+                <input type="password" id="inputPassword2" name="password" placeholder="Password" class="form-control" />
             </div>
 
             <div class="checkbox">
